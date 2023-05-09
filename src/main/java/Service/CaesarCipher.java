bb992c62de7fd27ad5865d0257eb742eb1032642
Susan Gicharu <SGicharu@safaricom.co.ke>
2023-05-09 13:03:40 +0300

    added cipher

--- src/main/java/Service/CaesarCipher.java
+++ src/main/java/Service/CaesarCipher.java
@@ -2,7 +2,7 @@ package Service;
 
 public class CaesarCipher {
     public string encode(){
-
+        //takes plainText as input and return cipherText
     }
     public string decode(){
         //takes cipherText as input and return plainText
