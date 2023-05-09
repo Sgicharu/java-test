2223bdc2d9889063aa925e818dec6f2025dd2d2f
Susan Gicharu <SGicharu@safaricom.co.ke>
2023-05-09 14:25:02 +0300

    added getters and setter

--- src/main/java/Model/userInput.java
+++ src/main/java/Model/userInput.java
@@ -4,12 +4,27 @@ public class userInput {
     private string operation;
     private String message;
     private int key;
-
     public string getOperation() {
         return operation;
     }
 
     public void setOperation(string operation) {
         this.operation = operation;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
     }
 }
